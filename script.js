e40cd98b203104b6e0b77cce67cc37129bbdcfb2
Alexander Crisler <alexcrisler@gmail.com>
2021-08-30 21:50:58 -0500

    1.0 etch-a-sketch working

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
 const container = document.getElementById("container");
+const element = document.querySelectorAll(".grid-item");
+
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+  }
+}
 
 function makeRows(rows, cols) {
+  removeAllChildNodes(container);
+
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
+
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
-    // cell.innerText = (c + 1);
     container.appendChild(cell).className = "grid-item";
+
+    cell.addEventListener("mouseover", event => {
+      cell.style.setProperty('background-color', 'black');
+    });
   };
 };
 
 makeRows(16, 16);
 
-const element = document.querySelectorAll(".grid-item");
+const clearbtn = document.getElementById("clear-btn");
 
-element.forEach( ele => ele.addEventListener("mouseover", event => {
-  ele.style.setProperty('background-color', 'black');
-}));
+function clearRows() {
+  element.forEach(ele => ele.style.setProperty('background-color', 'white'));
+
+  var size = prompt("How many pixels would you like?", 16);
+
+  if (size != null && size <= 1000 && size >= 1) {
+    makeRows(size, size);
+  } 
+}
+
+clearbtn.addEventListener("click", clearRows);
